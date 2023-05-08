713479eea08bded41a3e9a013941f179247b27f3
crayonzgrim <crayonzgrim@gmail.com>
2023-05-08 18:08:48 +0900

    :hammer: autoRotate, showing stars with drei

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ThreeScene from "./components/ThreeScene";
 import Sphere from "./components/Sphere";
+import {OrbitControls, Stars} from '@react-three/drei'
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Sphere color="#00ff00" position={[-2, 0, 0]}/>
         <Sphere color="#ff0000"  position={[2, 0, 0]}/>
         <ambientLight />
+        <OrbitControls autoRotate={true} />
+        <Stars />
       </ThreeScene>
     </div>
   );
