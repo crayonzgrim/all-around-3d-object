5b432614fee0da98b46dc77eed24e8eea8444828
crayonzgrim <crayonzgrim@gmail.com>
2023-05-08 17:51:05 +0900

    :tada: Initialized project & set prettier
    Initialized

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export const options = {
+  arrowParens: "always",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  trailingComma: "none",
+  bracketSpacing: true,
+};
+
+module.exports = options;
